db655fb9cdb7e766c3414d3576d9ea0bc0b3cedf
ashwinvj <ashwinjayanandan5@gmail.com>
2021-07-05 11:41:42 +0530

    Initial commit

---
+++ client/src/router.js
@@ -0,0 +1,42 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Home from './views/Home.vue'
+
+
+Vue.use(Router)
+
+const router = new Router({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes: [{
+      path: '/',
+      name: 'home',
+      component: Home
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: () => import('./views/Login.vue'),
+      meta: {
+        requiresGuest: true
+      }
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: () => import('./views/Register.vue'),
+      meta: {
+        requiresGuest: true
+      }
+    },
+    {
+      path: '/profile',
+      name: 'profile',
+      component: () => import('./views/Profile.vue'),
+      meta: {
+        requiresAuth: true
+      }
+    },
+  ]
+});
+export default router;
---
+++ client/src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <map-container></map-container>
+  </div>
+</template>
+
+<script>
+  import MapContainer from './mapContainer.vue';
+  
+  export default {
+    components: {
+      'map-container': MapContainer
+    }
+  }
+</script>;
